ac5db9edff1febb50f922402f46442e0ac672d67
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-02-19 07:38:46 +0700

    Fixed!

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -19,14 +19,14 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    f"Anda harus bergabung [Channel ini]({link}) untuk menggunakan saya , Sesudah gabung coba lagi !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("✨ Join Channel ✨", url=link)]
+                        [InlineKeyboardButton("• Join Channel •", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"Saya bukan admin dalam MUST_JOIN chat : {MUST_JOIN} !")
