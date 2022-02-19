b6a1dc141a8ef269de4abd63d545a6753b193c5e
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-02-19 07:35:55 +0700

    Fixed

--- StringSessionBot/help.py
+++ StringSessionBot/help.py
@@ -8,6 +8,6 @@ from pyrogram.types import InlineKeyboardMarkup
 async def _help(bot, msg):
     await bot.send_message(
         msg.chat.id,
-        "**Here's how to use me **\n" + Data.HELP,
+        "**Disini cara menggunakan saya **\n" + Data.HELP,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
     )
