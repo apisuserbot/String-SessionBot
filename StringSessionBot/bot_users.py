4b52140e9ac519dcf43118d5b64bc8e7b2b855e7
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-02-19 07:44:39 +0700

    Fixed id!

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1946995626) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(2081159749) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Pengguna : {users}", quote=True)
