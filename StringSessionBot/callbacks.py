78009970696650a8bc8d3124d420d0771eedf94b
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-02-19 07:42:58 +0700

    Fixed

--- StringSessionBot/callbacks.py
+++ StringSessionBot/callbacks.py
@@ -37,13 +37,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text="**Here's How to use me**\n" + Data.HELP,
+            text="**Disini cara menggunakan saya**\n" + Data.HELP,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.home_buttons),
         )
     elif query == "generate":
         await callback_query.message.reply(
-            "Silahkan Pilih String Mana Yang Ingin Kamu Ambil 😁",
+            "Silahkan Pilih String Mana Yang Ingin Anda Ambil",
             reply_markup=InlineKeyboardMarkup([[
                 InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
                 InlineKeyboardButton("Telethon", callback_data="telethon")
